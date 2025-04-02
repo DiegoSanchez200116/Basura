191c980774d4ecabc1be7455b06f162356070ea5
DiegoSanchez200116 <a01734778@tec.mx>
2025-04-01 18:34:48 -0600

    Agregar función de suma

--- basura.py
+++ basura.py
@@ -2,5 +2,10 @@ print('Hola a todos, este es un repositorio local y se desplegará en GitHub')
 
 print('Este archivo se estará modificando')
 
-for i in range(10):
+for i in range(10): 
     print(i)
+
+    def suma(a,b):
+        return a + b
+    
+    print(suma(98, 67))
